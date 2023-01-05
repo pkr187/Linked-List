0bea845fbc82c56cb4f51c43b0abca4a4dd2f82c
pkr187 <pankajroman187@gmail.com>
2023-01-05 23:41:18 +0530

    [pankaj] Add:CreateLinkedList

---
+++ LinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs\n 1.Create LinkedList");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        break;
+
+                }
+            }
+        }
+    }
+}
